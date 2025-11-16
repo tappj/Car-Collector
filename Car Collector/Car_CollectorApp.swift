aace80c27554026a4e517d1a4894a5960a7997b0
Jadon Tapp <tappj@uci.edu>
2025-11-15 21:34:50 -0800

    Initial Commit

---
+++ Car Collector/Car_CollectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Car_CollectorApp.swift
+//  Car Collector
+//
+//  Created by Jadon Tapp on 11/15/25.
+//
+
+import SwiftUI
+
+@main
+struct Car_CollectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
